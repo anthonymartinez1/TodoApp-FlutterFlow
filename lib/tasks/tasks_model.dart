e375ac1c79bc3a675ccfa7bc1d7756e92e2a78fa
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-02-18 23:48:16 +0000

    Implemented Inspirational Quote.

--- lib/tasks/tasks_model.dart
+++ lib/tasks/tasks_model.dart
@@ -1,12 +1,48 @@
+import '/backend/api_requests/api_calls.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/index.dart';
 import 'tasks_widget.dart' show TasksWidget;
 import 'package:flutter/material.dart';
 
 class TasksModel extends FlutterFlowModel<TasksWidget> {
+  ///  Local state fields for this page.
+
+  String? apiResponse1;
+
+  String? apiResponse2;
+
+  String? apiResponse3;
+
+  String? apiResponse4;
+
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for nameInput widget.
+  FocusNode? nameInputFocusNode;
+  TextEditingController? nameInputTextController;
+  String? Function(BuildContext, String?)? nameInputTextControllerValidator;
+  // Stores action output result for [Backend Call - API (helloName)] action in Button widget.
+  ApiCallResponse? apiResult9ax;
+  // State field(s) for squareInput widget.
+  FocusNode? squareInputFocusNode;
+  TextEditingController? squareInputTextController;
+  String? Function(BuildContext, String?)? squareInputTextControllerValidator;
+  // Stores action output result for [Backend Call - API (squareNumber)] action in Button widget.
+  ApiCallResponse? apiResultqx6;
+  // Stores action output result for [Backend Call - API (helloWorld)] action in helloWorld-Button widget.
+  ApiCallResponse? apiResult4ii;
+  // Stores action output result for [Backend Call - API (randomNumber)] action in randomNum-Button widget.
+  ApiCallResponse? apiResultc9y;
+
   @override
   void initState(BuildContext context) {}
 
   @override
-  void dispose() {}
+  void dispose() {
+    nameInputFocusNode?.dispose();
+    nameInputTextController?.dispose();
+
+    squareInputFocusNode?.dispose();
+    squareInputTextController?.dispose();
+  }
 }
